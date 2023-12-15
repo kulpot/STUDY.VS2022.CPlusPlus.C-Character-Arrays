8b805bf5d1ca1253857c875d6e324c8b88961aa1
Kulpot <kulpotwahu@gmail.com>
2023-12-15 13:18:36 +0800

    ...

--- C++ Character Arrays/CppScratchPad.cpp
+++ C++ Character Arrays/CppScratchPad.cpp
@@ -2,14 +2,17 @@
 // ref link:https://www.youtube.com/watch?v=Qvw2rNzUxDU&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=6
 
 #include <iostream>
-#include <string>
-using std::string;
+//#include <string>
+//using std::string;
 using std::cout;
 using std::endl;
 
 void main()
 {
 	char chars[] = "kulpot";		// implicit null terminator \0 which is the end of character string
-	string s = "kulpot";	// character array
-	cout << s << endl;
+	cout << chars << endl;
+	//char *chars = "kulpot";
+	//cout << chars << endl;
+	//string s = "kulpot";	// character array
+	//cout << s << endl;
 }
