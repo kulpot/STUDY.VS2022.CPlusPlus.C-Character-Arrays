59588c0d342692e194efbf3e8047364d71744a44
Kulpot <kulpotwahu@gmail.com>
2023-12-15 13:06:31 +0800

    Add project files.

---
+++ C++ Character Arrays/CppScratchPad.cpp
@@ -0,0 +1,3 @@
+// ------------------------------------- C++ Character Arrays -------------------------------
+// ref link:https://www.youtube.com/watch?v=Qvw2rNzUxDU&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=6
+
