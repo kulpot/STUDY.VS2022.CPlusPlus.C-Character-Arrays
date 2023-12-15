fc72e8eabb20f2c57a78a2e01fe87e4f296744ae
Kulpot <kulpotwahu@gmail.com>
2023-12-15 13:07:17 +0800

    ..

--- C++ Character Arrays/CppScratchPad.cpp
+++ C++ Character Arrays/CppScratchPad.cpp
@@ -1,3 +1,11 @@
 // ------------------------------------- C++ Character Arrays -------------------------------
 // ref link:https://www.youtube.com/watch?v=Qvw2rNzUxDU&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=6
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void main()
+{
+
+}
