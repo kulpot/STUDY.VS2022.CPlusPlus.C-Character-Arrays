91a0ec60786b081d76fa086eef40fbf9d99b5163
Kulpot <kulpotwahu@gmail.com>
2023-12-15 13:09:17 +0800

    ...

--- C++ Character Arrays/CppScratchPad.cpp
+++ C++ Character Arrays/CppScratchPad.cpp
@@ -2,10 +2,13 @@
 // ref link:https://www.youtube.com/watch?v=Qvw2rNzUxDU&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=6
 
 #include <iostream>
+#include <string>
+using std::string;
 using std::cout;
 using std::endl;
 
 void main()
 {
-
+	string s = "kulpot";
+	cout << s << endl;
 }
